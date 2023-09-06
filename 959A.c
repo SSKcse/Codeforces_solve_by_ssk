fc63dd3262f699a99935534235d3c20b55ac60c8
Shuvo Karmaker <sskshuvo.cse@gmail.com>
2023-09-06 13:00:59 +0600

    Add files via upload

---
+++ 959A.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+
+
+int a,b;
+scanf("%d",&a);
+
+//b= a%2 ? printf("Ehab\n") : printf("Mahmoud\n");
+
+if (a%2==1)
+{
+    printf("Ehab\n");
+}
+else
+{
+    printf("Mahmoud\n");
+}
+
+return 0;
+
+}
+
+
+
